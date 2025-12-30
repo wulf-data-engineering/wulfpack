58e28208d90a4c379602595e31c23868934b0c70
Christoph Wulf <christoph@wulf.technology>
2025-12-30 22:23:32 +0100

    Upgrade storybook

--- template/%[cookiecutter.project_slug]%/frontend/.storybook/main.ts
+++ template/%[cookiecutter.project_slug]%/frontend/.storybook/main.ts
@@ -4,7 +4,7 @@ const config: StorybookConfig = {
 	framework: '@storybook/sveltekit',
 	stories: ['../src/**/*.stories.@(js|ts|svelte)'],
 	staticDirs: ['../src/lib/assets'],
-	addons: ['@storybook/addon-essentials', '@storybook/addon-svelte-csf', '@storybook/addon-docs']
+	addons: ['@storybook/addon-svelte-csf', '@storybook/addon-docs']
 };
 
 export default config;
